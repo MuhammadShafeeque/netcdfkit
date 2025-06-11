a25cbe7ac09c5bc6bde4a510425f629b8401e4aa
MuhammadShafeeque <shafeequ@uni-bremen.de>
2025-06-11 11:22:01 +0200

    bump version to 0.1.1

--- src/netcdfkit/__init__.py
+++ src/netcdfkit/__init__.py
@@ -8,7 +8,7 @@ from large NetCDF files at specific geographic points and polygon regions.
 from .ncPntExtractor import NetCDFPointExtractor
 from .ncPolyExtraction import NetCDFPolygonExtractor
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Muhammad Shafeeque"
 __email__ = "muhammad.shafeeque@awi.de | shafeequ@uni-bremen.de"
 
